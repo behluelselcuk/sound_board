1d2acf19ead84c31baea8fc18307b6f73b673900
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-03-19 15:21:41 +0100

    Setze JS um
    Die Funktionalität beim Klick auf einen Btn wurde
    umgesetzt.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict'
+
+// Ziel
+// wenn man auf einen btn klickt
+//     dann soll die aktuell laufende audio gestoppt werden
+//     dann soll die zugehörige audio abgespielt werden
+
+
+// Eingabe
+const buttons = document.querySelectorAll('.btn')
+
+
+// Verarbeitung
+buttons.forEach(button => {
+    // Beim Klick auf ein Button
+    button.addEventListener('click', () => {
+        // Audio-Elemente auswähen & stoppen
+        const audios = document.querySelectorAll('audio');
+        audios.forEach(audio => {
+            audio.pause();
+            // Playback auf den Anfang setzen
+            audio.currentTime = 0;
+        });
+    
+        // Zuweisen des zugehörigen Audio-Elements durch Btn-Namen
+        const audioId = button.textContent.trim();
+        const audioToPlay = document.querySelector(`#${audioId}`);
+        // Audio-Element abspielen
+        audioToPlay.play();
+
+    
+    })
+});
